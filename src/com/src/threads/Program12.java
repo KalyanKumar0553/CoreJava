c01dfbbeff9e39f0ffb21195649a1d023db5e89b
KTIRUVEE <KTIRUVEE@LBNL12026.corp.capgemini.com>
2018-04-24 20:58:32 +0200

    Own Thread Pool

--- src/com/src/threads/Program12.java
+++ src/com/src/threads/Program12.java
@@ -1,11 +1,12 @@
 package com.src.threads;
 
+import java.util.ArrayDeque;
+
 class Task implements Runnable {
 	private String name;
 	Task(int i) {
 		this.name = "Worker "+i;
 	}
-	
 	@Override
 	public void run() {
 		try {
@@ -17,9 +18,57 @@ class Task implements Runnable {
 		}
 	}
 }
-
+class MyThreadPool extends Thread {
+	private ArrayDeque<Task> tasks;
+	private Boolean process;
+	private Executor[] executors;
+	private int size;
+	
+	@Override
+	public void run() {
+		for(int i=0;i<this.executors.length;i++) {
+			this.executors[i] = new Executor();
+			Thread t = new Thread(this.executors[i]);
+			t.setName("Executor "+i);
+			t.start();
+		}
+	}
+	
+	public MyThreadPool(int size) {
+		this.size = size;
+		this.executors = new Executor[this.size];
+		this.process = true;
+		this.tasks = new ArrayDeque<>();
+	}
+	
+	public void addTask(Task task) {
+		synchronized(tasks) {
+			tasks.add(task);
+		}
+	}
+	
+	public void terminate() {
+		this.process = false;
+	}
+	
+	class Executor implements Runnable {
+		private Task currTask;
+		@Override
+		public void run() {	
+			while(process) {
+				while((currTask=tasks.poll())!=null) {
+					currTask.run();
+				}
+			}
+		}
+	}
+}
 class Program12 {
 	public static void main(String[] args) throws Exception{
-		
+		MyThreadPool pool = new MyThreadPool(2);
+		pool.run();
+		for(int i=0;i<10;i++) {
+			pool.addTask(new Task(i));
+		}
 	}
 }
